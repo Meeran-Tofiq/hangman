33cf00bdd8a9310c948d0fdc9c67e5e7460a3ae2
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-03-23 13:20:11 +0300

    Add player class with ability to make a guess

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,14 +1,16 @@
 WORDS = File.readlines('google-10000-english-no-swears.txt', chomp: true)
 
 class Game
+    attr_reader :board, :player
     def initialize
         @word = self.pick_random_word
         @board = Board.new(@word)
+        @player = Player.new(@board)
     end
 
     def play
-        @board.display_board
-        return true
+        board.display_board
+        return false
     end
 
     def pick_random_word
@@ -23,18 +25,47 @@ class Game
 end
 
 class Board
+    attr_reader :board, :guesses, :word
     def initialize(word)
+        @word = word
         @board = Array.new(word.length) {"_ "}
         @guesses = Array.new
     end
 
     def display_board
-        @board.each do |letter|
+        board.each do |letter|
             print letter
         end
         puts "\n\n"
     end
 end
 
-game = Game.new
-game.play
+class Player
+    attr_reader :board
+    def initialize(board)
+        @board = board
+    end
+
+    def make_guess(guess)
+        guess = guess.downcase
+
+        if guesses.include?(guess) || !("a".."z").include?(guess)
+            return false
+        else
+            if word.include?(guess)
+                board[word.index(guess)] = guess.upcase + " "
+            else
+                puts "That is incorrect!"
+            end
+        end
+
+        return true
+    end
+end    
+
+replay = true
+
+while replay
+    game = Game.new
+    replay = game.play
+end
