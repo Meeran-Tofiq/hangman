58d01e4dc06dfe062c3616bba0ca911c96b533fc
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-03-23 13:40:15 +0300

    Add ability to replay

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,14 @@
 WORDS = File.readlines('google-10000-english-no-swears.txt', chomp: true)
+GAME_RULES = "This is a game of Hangman!
+
+The computer selects a random word that has anywhere between 5 and 12 letters,
+and the player has to guess the word by guessing the letters in it.
+
+The player has 7 tries after which you'd lose. You can play another game after
+that, or leave it.
+
+This game also has the ability of saving, at the beginning you'll be queried f-
+or a file if you want to load that file. Otherwise, you can just play normally."
 
 class Game
     attr_reader :board, :player
@@ -10,7 +20,14 @@ class Game
 
     def play
         board.display_board
-        return false
+        win = false
+        puts GAME_RULES
+
+        until win
+            break
+        end
+
+        prompt_replay
     end
 
     def pick_random_word
@@ -22,20 +39,27 @@ class Game
         
         word
     end
+
+    def prompt_replay
+        puts "Do you want to play again? (y/...)"
+        replay = gets.chomp.downcase
+
+        return (replay == 'y' ? true : false)
+    end
 end
 
 class Board
     attr_reader :board, :guesses, :word
     def initialize(word)
         @word = word
-        @board = Array.new(word.length) {"_ "}
-        @guesses = Array.new
+        @board = Array.new(word.length) {"_"}
+        @guesses = Array.new(7) {"O"}
     end
 
     def display_board
-        board.each do |letter|
-            print letter
-        end
+        board.each {|letter| print letter + " "}
+        puts "\n"
+        guesses.each {|guess| print guess + " " }
         puts "\n\n"
     end
 end
@@ -53,9 +77,10 @@ class Player
             return false
         else
             if word.include?(guess)
-                board[word.index(guess)] = guess.upcase + " "
+                board.board[word.index(guess)] = guess.upcase
             else
                 puts "That is incorrect!"
+                board.guesses[board.guesses.index("O")] = "X"
             end
         end
 
